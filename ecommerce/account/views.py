b5576a39fc6c515dea9d7c074514e8c7727a8fd8
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-10-11 19:34:24 -0300

    Aula 62

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -7,14 +7,26 @@ def register(request):
     form = CreateUserForm()
 
     if request.method == 'POST':
-
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
             form.save()
 
-            return redirect()
+            return redirect('store')
 
     context = {'form': form}
 
     return render(request, 'account/registration/register.html', context=context)
+
+
+def email_verification(request):
+    pass
+
+def email_verification_sent(request):
+    pass
+
+def email_verification_success(request):
+    pass
+
+def email_verification_failed(request):
+    pass
